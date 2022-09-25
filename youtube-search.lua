85089ce2eb09a24f718a9b1af501a7681b616ae4
willswats <williamstuwatson@gmail.com>
2022-09-25 01:08:48 +0100

    Cleanup code

--- youtube-search.lua
+++ youtube-search.lua
@@ -22,16 +22,11 @@ local function search_youtube(user_input)
     local search_command = "ytdl://ytsearch"
     local search_query = opts.search_results .. ":" .. user_input
 
-    mp.commandv("loadfile", search_command .. search_query, "replace")
+    mp.commandv("loadfile", search_command .. search_query)
 end
 
 local function open_search()
-    local get_user_input_options = {
-        request_text = "Enter search text:",
-        replace = true
-    }
-
-    input.get_user_input(search_youtube, get_user_input_options, "replace")
+    input.get_user_input(search_youtube, {request_text = "Enter search text:"})
 end
 
 mp.add_key_binding(opts.key_opensearch, "open-search", open_search)
