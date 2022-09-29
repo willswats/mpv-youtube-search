a2eddc0cfdd0ea708c80bde640a88d3563390b1b
willswats <williamstuwatson@gmail.com>
2022-09-29 23:27:12 +0100

    Rename open_search to search_youtube, combine open_search and search_youtube functions, add osd message, add osd_message_duration option, create is_whitespace helper function

--- youtube-search.lua
+++ youtube-search.lua
@@ -6,28 +6,44 @@ package.path = mp.command_native({"expand-path", "~~/script-modules/?.lua;"}) ..
 local input = require "user-input-module"
 
 local opts = {
-    -- Key to open search
-    key_open_search = "ALT+s",
+    -- Key to open the input for searching youtube
+    key_search_youtube = "ALT+s",
 
     -- Number of search results
-    search_results = 50
+    search_results = 50,
+
+    -- Duration of osd messages
+    osd_message_duration = 5
 }
 
 -- Read configuration file
 options.read_options(opts, "youtube-search")
 
-local function search_youtube(user_input)
-    -- End if user_input does not exist or if user_input is an empty string (after removing whitespace)
-    if not user_input or string.gsub(user_input, "%s+", "") == "" then return end
-
-    local search_command = "ytdl://ytsearch"
-    local search_query = opts.search_results .. ":" .. user_input
-
-    mp.commandv("loadfile", search_command .. search_query)
+-- Check if string is whitespace
+local function is_whitespace(str)
+    if string.gsub(str, "%s+", "") == "" then
+        return true
+    else
+        return false
+    end
 end
 
-local function open_search()
-    input.get_user_input(search_youtube, {request_text = "Enter search text:"})
+local function search_youtube()
+    input.get_user_input(function(user_input)
+        if not user_input then
+            return
+        elseif is_whitespace(user_input) then
+            mp.osd_message("Error: Input must include text",
+                           opts.osd_message_duration)
+            return
+        end
+
+        local search_command = "ytdl://ytsearch"
+        local search_query = opts.search_results .. ":" .. user_input
+
+        mp.commandv("loadfile", search_command .. search_query)
+    end, {request_text = "Enter search text:"})
 end
 
-mp.add_key_binding(opts.key_open_search, "open_search", open_search)
+-- Add key bindings
+mp.add_key_binding(opts.key_search_youtube, "search_youtube", search_youtube)
