6f4ec49ddc3a3c19c3e3ba318251bde458038f0f
willswats <william@williamwatson.dev>
2026-02-19 13:05:03 +0000

    feat: remove flake-utils

--- flake.nix
+++ flake.nix
@@ -3,19 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages = {
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
           mpv-youtube-search = pkgs.callPackage ./default.nix { };
           default = self.packages.${system}.mpv-youtube-search;
-        };
-      }
-    );
+        }
+      );
+    };
 }
